64bc15e3066b8e6ab46c8b5bc31d641605615451
James Carr <jc3212@ic.ac.uk>
2016-02-15 16:05:22 +0000

    Added basic templates and static content

--- devfun/views.py
+++ devfun/views.py
@@ -1,10 +1,15 @@
 from devfun import app
+from flask import send_from_directory, render_template
+
+@app.route('/static/<filename>')
+def serve_static_file(filename):
+    return send_from_directory(app.static_folder, filename)
+
 
 @app.route('/')
-def hello():
-    return "Goodbye!"
+def index():
+    return render_template('index.html')
 
-
-@app.route('/<name>')
-def hi(name):
-    return "Goodbye! {}".format(name)
+@app.errorhandler(404)
+def error_handler(error):
+    return render_template('404.html'), 404
