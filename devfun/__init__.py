e58291c749c3fc440aa4fa8a08d3ec2e5c0afffa
James Carr <jc3212@ic.ac.uk>
2016-02-15 21:39:35 +0000

    Implemented User logins

--- devfun/__init__.py
+++ devfun/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 app = Flask(__name__, static_folder="static")
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.login_view = "/register"
+lm.init_app(app)
 
 from devfun import views, models
--- devfun/views.py
+++ devfun/views.py
@@ -1,7 +1,17 @@
-from devfun import app, db
-from flask import send_from_directory, render_template, request, flash, redirect, url_for
+from devfun import app, db, lm
+from flask import send_from_directory, render_template, request, flash, redirect, url_for, g
+from flask.ext.login import current_user, login_user, login_required
 from .forms import LoginForm
 from .models import User
+
+@lm.user_loader
+def load_user(id):
+    print "Got here LOGLOGLOG"
+    return User.query.get(id)
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/static/<filename>')
 def serve_static_file(filename):
@@ -9,18 +19,23 @@ def serve_static_file(filename):
 
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html', title="DevFun home page!")
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User(form.username.data, form.email.data,
                   form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        login_user(user)
+
         flash('Thanks for registering')
         return redirect(url_for('index'))
     return render_template('register.html', form=form, title="Register Account")
