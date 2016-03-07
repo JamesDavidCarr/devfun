631feb4b00496cbcb313cae96dae85f509f14143
James Carr <jdcarrman+github@gmail.com>
2016-03-07 15:52:07 +0000

    Added some css styling to the site

--- devfun/views.py
+++ devfun/views.py
@@ -17,7 +17,7 @@ def serve_static_file(filename):
     return send_from_directory(app.static_folder, filename)
 
 @app.errorhandler(404)
-def error_handler():
+def error_handler(error):
     return render_template('404.html'), 404
 
 
